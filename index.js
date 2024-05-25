3ad6216a5e9679bb8924c93b75a78e0b1e7cf1ba
Forz70043 <alfonso.pisicchio@libero.it>
2024-05-25 11:59:42 +0200

    Included db, env

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
 
 const express = require('express');
+require('dotenv').config();
+let db = require('./db');
 const app = express();
-const port = 3000;
 
 app.get('/', (req, res) => {
   res.send('Hello World!');
 })
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+app.listen(process.env.APP_PORT, () => {
+  console.log(`Example server listening on port ${process.env.APP_PORT}`);
 })
 
