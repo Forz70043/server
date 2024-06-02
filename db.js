6b3ab0fb1dac5d65950b6f4a4a80bcba837d1a47
Forz70043 <alfonso.pisicchio@libero.it>
2024-06-02 09:08:11 +0200

    Refactor db

--- db.js
+++ db.js
@@ -1,19 +1,20 @@
 const { Sequelize } = require('sequelize');
 require('dotenv').config();
 
-let db = new Sequelize(process.env.DB_NAME, process.env.DB_USERNAME, process.env.DB_PASS, {
-    host: process.env.DB_HOST,
-    dialect: 'mysql'
-});
-
-async function isConnected() {
-    try {
-        await db.authenticate();
-        console.log('Connection has been established successfully.');
-    } catch (error) {
-        console.error('Unable to connect to the database:', error);
+const sequelize = new Sequelize(
+    process.env.DB_NAME, 
+    process.env.DB_USERNAME, 
+    process.env.DB_PASS, 
+    {
+        host: process.env.DB_HOST,
+        dialect: 'mysql',
     }
+);
+
+const modelDefiners = [];
+
+for(let modelDefiner of modelDefiners) {
+    modelDefiner(sequelize);
 }
 
-
-module.exports = {db, isConnected};
+module.exports = sequelize;
